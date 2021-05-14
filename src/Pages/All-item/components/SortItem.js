fb9dd5e48098575c5731aaa12baae323e04228e2
DDS-NamLP <lenam.khmt@gmail.com>
2021-05-15 03:26:10 +0700

    /

--- src/Pages/All-item/components/SortItem.js
+++ src/Pages/All-item/components/SortItem.js
@@ -3,23 +3,7 @@ import React from "react";
 const SortItem = (props) => {
   const { color } = props;
 
-  return (
-    <>
-      <div>
-        <div className="all-header" style={{ color }}>
-          Sắp Xếp:
-          <select className="col l-2 all-select">
-            <option value>Mặc định</option>
-            <option value>Gía tăng dần</option>
-            <option value>Gía giảm dần</option>
-            <option value>Từ A đến Z</option>
-            <option value>Từ Z đến A</option>
-          </select>
-        </div>
-      </div>
-      <div className="boxx" style={{ background: color }}></div>
-    </>
-  );
+  return <div className="boxx" style={{ background: color }}></div>;
 };
 
 export default SortItem;
